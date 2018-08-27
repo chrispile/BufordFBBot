7c22c59b95267a27329f2166160e5f7d6af7a3ab
Chris <pilec@uci.edu>
2018-08-27 11:30:48 -0700

    Added FB verification callback

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+    console.log('Webhook server is listening, port 3000');
+})
+
+const verificationController = require('./controllers/verification');
+
+app.get('/', verificationController);
